1658828c806b00d9e41d43cf04e8f8a4af54d5ee
Spep <ilyaesegj@gmail.com>
2021-12-07 11:26:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+int N;
+int k;
+int main() 
+{
+    setlocale(LC_ALL,"Russian");
+    cout << "Количество чисел в последовательности:";
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cout << i+1<< " ";
+    }
+    cout << endl;
+    cout << "Удалить элементы кратные:";
+    cin >> k;
+    cout << endl;
+    cout << "результат:";
+
+       for (int i = 0; i < N; i++)
+        {
+            if(i % k)
+            {
+                cout << i << " ";
+            }
+            else
+            {
+            cout << "";
+            }
+
+        }
+}
